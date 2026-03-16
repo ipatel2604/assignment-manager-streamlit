b04c40448fb6d432477e8ac1d168c69ccc4ddc0e
Ishan Patel <ishanpatel@wifi-roaming-128-4-81-87.wifi.udel.edu>
2026-03-16 10:35:20 -0400

    changes

--- app_day3.py
+++ app_day3.py
@@ -2,6 +2,22 @@ import streamlit as st
 import time
 import json 
 from pathlib import Path
+assignment= [
+    {
+        "id": "HW1",
+       "title": "Introduction to database",
+       "description": "basis of database design",
+       "points": 100,
+       "type":"homework"
+    },
+    {
+        "id":"HW2",
+        "title": "Normalization",
+        "description": "Normalize the table design",
+        "point": 100,
+        "type": "lab"
+    }
+]
 
 st.set_page_config(
     page_title="Course Manager",
@@ -27,3 +43,10 @@ assignment= [
     }
 ]
 json_path = Path("Assignment.json")
+
+
+elif st.session_state["role"]=="student":
+    if st.session_state ["page"] =="home"
+        st.markdown(f"welcome {st.session_state['user']['email']}" )
+        if st.button("goto dashboard", type="primary"key= "view_dash_btn"):
+        
